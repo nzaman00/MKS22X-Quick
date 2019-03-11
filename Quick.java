52d75e6c638604ce949e48516de668e8df7a4c41
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-11 10:13:36 -0400

    Update Quick.java

--- Quick.java
+++ Quick.java
@@ -1,7 +1,43 @@
+import java.util.Random;
 public class Quick{
+  public static int partition(int[] ary, int start, int end){
+    Random randgen = new Random();
+    int index = Math.abs(randgen.nextInt()) % (end - start);
+    int hold = ary[start];
+    ary[start] = ary[index];
+    ary[index] = hold;
+    index = start;
+    int point = end;
+    for(int i = start + 1; i < end; i++){
+      System.out.println(toString(ary));
+      if(ary[i] <= ary[index]){
+        hold = ary[i];
+        ary[i] = ary[index];
+        ary[index] = hold;
+        index ++;}
+      else{
+        while(ary[point] > ary[index]){
+          if(point == index + 1){
+            return index;}
+          point --;}
+        hold = ary[point];
+        ary[point] = ary[i];
+        ary[i] = hold;
+        i--;}}
+    return index;}
+    public static String toString(int[] ary){
+      String output = "";
+      for(int i = 0; i < ary.length; i++){
+        output += ary[i] + " ";}
+      return output;
+    }
+
+  
   /*return the value that is the kth smallest value of the array.
  */
- public static int quickselect(int []data, int k){ }
+ public static int quickselect(int []data, int k){
+  
+ }
 
 
 }
