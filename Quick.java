594e4485bc41aebeb88bd27736171faf951cd0a0
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-13 10:12:47 -0400

    fixed partition

--- Quick.java
+++ Quick.java
@@ -1,39 +1,50 @@
 import java.util.Random;
-public class Quick{
-  public static int partition(int[] ary, int start, int end){
-    Random randgen = new Random();
-    int index = Math.abs(randgen.nextInt()) % (end - start);
-    int hold = ary[start];
-    ary[start] = ary[index];
-    ary[index] = hold;
-    index = start;
-    int point = end;
-    for(int i = start + 1; i < end; i++){
-      System.out.println(toString(ary));
-      if(ary[i] <= ary[index]){
-        hold = ary[i];
-        ary[i] = ary[index];
-        ary[index] = hold;
-        index ++;}
-      else{
-        while(ary[point] > ary[index]){
-          if(point == index + 1){
-            return index;}
-          point --;}
-        hold = ary[point];
-        ary[point] = ary[i];
-        ary[i] = hold;
-        i--;}}
-    return index;}
-    public static String toString(int[] ary){
-      String output = "";
-      for(int i = 0; i < ary.length; i++){
-        output += ary[i] + " ";}
-      return output;
-    }
-    public static void main(String[] args) {
-      int[] ary = {4,10,3,7,6,8,9,1,12,15,16,24};
-      System.out.print(partition(ary, 0, 11));
-
-    }
+public class test{
+  public static String toString(int[] ary){
+    String output = "";
+    for(int i = 0; i < ary.length; i++){
+      output += ary[i] + " ";}
+    return output;
   }
+  public static int partition (int [] data, int start, int end){
+   Random rand = new Random();
+   int pick = rand.nextInt(data.length);
+   int index = 0;
+   int pivot = data[pick];
+   data[pick] = data[0];
+   data[0] = pivot;
+   while (start != end) {
+     int value = data[start];
+     if (value > pivot) {
+       data[start] = data[end];
+       data[end] = value;
+       end--;
+     }
+     if (value <= pivot) {
+       start++;
+     }
+   }
+   if (start == 0 && end == 0) {
+     return index;
+   }
+   if (start == end) {
+     if (pivot <= data[start]) {
+       data[0] = data[start - 1];
+       data[start - 1] = pivot;
+       index = start - 1;
+     } else {
+       data[0] = data[start];
+       data[start] = pivot;
+       index = start;
+     }
+   }
+   System.out.println(pivot);
+   System.out.println(toString(data));
+   return index;
+  }
+public static void main(String[] args) {
+  int[] ary = {23,10,36,7,6,8,73,1,124,15,16,24};
+  System.out.print(partition(ary, 0, 11));
+
+  } 
+}
